b67a0130a276b14466e434813284bfcfa3f9a1d6
Fulya Cimendere <fulyacoskun.87@gmail.com>
2023-07-16 15:08:05 +0200

    add rock_paper_scissors.py file

---
+++ Day #04 - Game : Rock Paper Scissors/rock_paper_scissors.py
@@ -0,0 +1,100 @@
+"""
+PRACTISE : Randomisation and Python Lists
+GAME : Rock Paper Scissors 
+Rock beats scissors, scissors beat paper, and paper beats rock.
+Agree ahead of time whether you’ll count off “rock, paper, scissors, shoot” or just “rock, paper, scissors.”
+Use rock, paper, scissors to settle minor decisions or simply play to pass the time.
+"""
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+stop = '''
+   __
+ /    \
+| STOP |
+ \ __ /
+   ||
+   ||
+ ~~~~~~
+'''
+
+
+#collect the user selection (rock,paper or scissors)
+my_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+
+hand_options = [rock, paper, scissors]
+num_hand_options = len(hand_options)
+
+# find user selection and print on screen
+print("Your chose: ")
+for i in range (num_hand_options):
+  if my_choice == i:
+    print(hand_options[i])
+
+
+# create pc selection and print out on screen 
+if my_choice < 0 or my_choice>=3:
+  print(f"You typed invalid number,you lose!\n{stop}")
+else:
+    pc_choice = random.randint(0,2)
+    print("Computer chose: ")
+    if pc_choice == 0:
+        print(hand_options[pc_choice])
+    
+    elif pc_choice == 1:
+        print(hand_options[pc_choice])
+    
+    else: 
+        print(hand_options[pc_choice])
+    # control of the conditions of the game rules👇
+    # game rules👇
+    # Rock  - 0wins against scissors.
+    # Scissors -2 win against paper.
+    # Paper wins-1 against rock.
+
+    if my_choice == pc_choice:
+        print("Draw")
+    elif my_choice == 0 and pc_choice == 1:
+        print("you lose")
+    elif my_choice == 0 and pc_choice == 2:
+        print("you win")
+    elif my_choice == 1 and pc_choice == 0:
+        print("you win")
+    elif my_choice == 1 and pc_choice == 2:
+        print("you lose")
+    elif my_choice == 2 and pc_choice == 0:
+        print("you lose")
+    elif my_choice == 2 and pc_choice == 1:
+        print("you win")
+
+
+
+
+  
