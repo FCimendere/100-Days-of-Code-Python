73bc4f46bdcf3b9ffce0f879fcd715f5b3cb8406
Fulya Cimendere <fulyacoskun.87@gmail.com>
2023-09-20 16:43:51 +0200

    update snake folder to hold the highest score in the data.txt file.

--- Day #21/food.py
+++ Day #21/food.py
@@ -6,10 +6,10 @@ class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("turtle")
+        self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.8, stretch_wid=0.8)
-        self.color("red")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
         self.speed("fastest")
         self.refresh()
 
